802590967201376595e0c0f2db20c41ff022c379
Olivier Binette <olivier.binette@gmail.com>
2023-11-25 14:24:24 -0500

    tweaks to documentation

--- bettercsv/write.py
+++ bettercsv/write.py
@@ -12,6 +12,7 @@ def write(
     schema: Optional[dict] = None,
     header: bool = False,
     dialect: str | dict = DEFAULT_DIALECT,
+    **additional_metadata
 ) -> None:
     """
     Write CSV data and optional metadata.
@@ -28,9 +29,11 @@ def write(
         Flag to indicate if the first row of the data is a header, by default True.
     dialect : str | dict, optional
         The dialect to be used for writing the CSV file, by default DEFAULT_DIALECT (unix). See https://docs.python.org/3/library/csv.html#csv-fmt-params for valid parameters and more information.
+    additional_metadata : dict, optional
+        Additional metadata to be written to the metadata file.
     """
     _write_csv(dirpath, rowsdata, dialect)
-    _write_metadata(dirpath, schema, header, dialect)
+    _write_metadata(dirpath, schema, header, dialect, **additional_metadata)
 
 
 def _write_csv(dirpath: str, rowsdata: Iterable[Iterable[Any]], dialect: str | dict = DEFAULT_DIALECT) -> None:
@@ -60,8 +63,7 @@ def _write_csv(dirpath: str, rowsdata: Iterable[Iterable[Any]], dialect: str | d
 
 
 def _write_metadata(
-    dirpath: str, schema: Optional[dict] = None, header: bool = False, dialect: str | dict = DEFAULT_DIALECT
-) -> None:
+    dirpath: str, schema: Optional[dict] = None, header: bool = False, dialect: str | dict = DEFAULT_DIALECT, **additional_metadata) -> None:
     """
     Internal function to write metadata associated with a CSV file.
 
@@ -75,8 +77,11 @@ def _write_metadata(
         Flag to indicate if the first row of the data is a header, by default True.
     dialect : str | dict, optional
         The dialect used for the CSV file, by default DEFAULT_DIALECT.
+    additional_metadata : dict, optional
+        Additional metadata to be written to the metadata file.
     """
     metadata = {"schema": schema, "header": header, "dialect": dialect}
+    metadata.update(additional_metadata)
 
     metadata_filepath = os.path.join(dirpath, "metadata.json")
     with open(metadata_filepath, "w") as metafile:
